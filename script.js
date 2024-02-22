6446108d063ca49e0ac1dd5ed8bc12b63febd6ff
Loknath Yadav <101386138+Lokiyadav@users.noreply.github.com>
2024-02-22 15:54:38 +0530

    loader and half page 1

---
+++ script.js
@@ -0,0 +1,81 @@
+
+function LoadingAnimation(){
+  var tl = gsap.timeline();
+
+tl.from(".line h1", {
+  y: 200,
+  duration: 0.6,
+  stagger: 0.20,
+  delay: 0.1,
+});
+
+tl.from(".line1-par1", {
+  opacity: 0,
+  onStart: function () {
+    var load = document.querySelector(".line1-par1 h5");
+    var count = 0;
+    setInterval(function () {
+      if (count < 100) {
+        count++;
+        load.innerHTML = count;
+      } else {
+        load.innerHTML = count;
+      }
+    }, 30);
+  },
+});
+
+tl.to(".line h2",{
+    animationName:"anime",
+    opacity:1,
+})
+
+tl.to("#loader",{
+  opacity:0,
+  duration:0.2,
+  delay:2.3
+})
+
+tl.from("#page1",{
+  delay:0.2,
+  y:1600,
+  opacity:0,
+  duration:0.5,
+  ease:Power3,
+})
+
+tl.to("#loader",{
+    display:"none",
+})
+tl.from(".nav-part2 h2",{
+  y:100,
+  stagger:0.2,
+})
+tl.from(".page1-center h1",{
+   y:150,
+  stagger:0.1,
+  duration:0.3
+})
+tl.from("#page1-center3 h2,#page1-center3 h3",{
+  y:150,
+  stagger:0.1,
+  duration:0.1
+})
+}
+LoadingAnimation();
+function CursorAnimation(){
+  document.addEventListener("mousemove",function(position){
+    gsap.to("#crsr",{
+     left:position.x,
+     top:position.y
+    })
+ })
+ 
+ 
+ Shery.makeMagnet(".nav-part2 h2" /* Element to target.*/, {
+   //Parameters are optional.
+   // ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+   // duration: 1,
+ });
+}
+CursorAnimation();
