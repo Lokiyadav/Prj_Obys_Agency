8f07666bc622322d33de7852ffff7c70856f0a8b
Loknath Yadav <101386138+Lokiyadav@users.noreply.github.com>
2024-02-24 09:51:02 +0530

    page4 page5 pag6 design and page1 and page2 small  animation

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+//locomotive applying
+function locomotivescrolltrAnimation(){
+  gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+locomotivescrolltrAnimation();
 
 function LoadingAnimation(){
   var tl = gsap.timeline();
@@ -33,9 +67,9 @@ tl.to(".line h2",{
 tl.to("#loader",{
   opacity:0,
   duration:0.2,
-  //edit this section
-  // delay:2.3
-  delay:1
+  // edit this
+  // delay:2.3,
+  delay:0,
 })
 
 tl.from("#page1",{
@@ -65,6 +99,7 @@ tl.from("#page1-center3 h2,#page1-center3 h3",{
 })
 }
 LoadingAnimation();
+
 function CursorAnimation(){
   document.addEventListener("mousemove",function(position){
     gsap.to("#crsr",{
